201eaa59c1556ff27c50cc0d7eb6b0203fe0ee55
Jakub Sajnóg <jakubsajnog@gmail.com>
2025-03-26 18:08:52 +0100

    create Header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,40 @@
+import styles from './Header.module.css';
+import Logo from '../logo/Logo.tsx';
+import Separator from '../separtor/Separator.tsx';
+import NavItem from '../navItem/NavItem.tsx';
+import Button from '../button/Button.tsx';
+import { useNavigate } from 'react-router-dom';
+
+const Header = () => {
+  const navLinks = [
+    { to: '/', label: 'Strona główna' },
+    { to: '/about', label: 'O Nas' },
+    { to: '/contact', label: 'Nasze Usługi' },
+    { to: '/contact', label: 'Kontakt' },
+  ];
+
+  const navigate = useNavigate();
+  return (
+    <>
+      <header className={styles.headerWrapper}>
+        <Logo />
+        <div className={styles.center}>
+          <nav className={styles.navigation}>
+            {navLinks.map((link) => (
+              <NavItem key={link.to} to={link.to} label={link.label} />
+            ))}
+          </nav>
+        </div>
+        <div className={styles.right}>
+          <Button
+            label={'Umów darmową konsultację!'}
+            onClick={() => navigate('/formularz-kontaktowy')}
+          />
+        </div>
+      </header>
+      <Separator />
+    </>
+  );
+};
+
+export default Header;
